b3e6db80594da9b9f2527fd9b7d86bb9a70ac548
alex <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>
2025-08-26 11:08:41 +0300

    demo

---
+++ unsubscribe-successful.php
@@ -0,0 +1,13 @@
+<html>
+<meta charset="UTF-8" />
+<head>
+    <title>Unsubscribe successful</title>
+</head>
+<body>
+<div>
+    <h4>Unsubscribe successful</h4>
+    <br />
+    <a href="/email-unsubscribe.php">To unsubscribe page</a>
+</div>
+</body>
+</html>
---
+++ unsubscribe-unsuccessful.php
@@ -0,0 +1,16 @@
+<html>
+<meta charset="UTF-8" />
+<head>
+    <title>Unsubscribe unsuccessful</title>
+</head>
+<body>
+<div>
+    <h4>Unsubscribe unsuccessful</h4>
+    <?php
+    echo "<h5>" . urldecode($_GET['error']) . "</h5";
+    ?>
+    <br/>
+    <a href="/email-unsubscribe.php">To unsubscribe page</a>
+</div>
+</body>
+</html>
